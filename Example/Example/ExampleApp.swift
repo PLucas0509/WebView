1c7f0f0dbc44238da01afe7445f955baecb61db1
PLucas0509 <89928055+PLucas0509@users.noreply.github.com>
2022-11-10 14:30:12 +0100

    Add files via upload

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  WebView
+//
+//  Created by Ben Chatelain on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
